d9c11e3d077236c72c6338635457a7b2c9342dd1
Tim <yyan0195@uni.sydney.edu.au>
2022-05-11 19:57:17 +1000

    trying procmask during reporting to make it atomic to signals

--- spx_exchange.c
+++ spx_exchange.c
@@ -736,7 +736,7 @@ void report_position_for_trader(trader* t){
 }
 
 // TODO: Fix up race condition in output during reporting
-//Test 7
+//Test 8
 // Reports the order boook and positions for every product/trader on the exchange
 void report(exch_data* exch){
 	sigset_t s;
